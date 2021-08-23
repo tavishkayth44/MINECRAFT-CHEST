61db19e2fdca0eb56681d759bb6042557fa4c2d2
tavishkayth44 <81311125+tavishkayth44@users.noreply.github.com>
2021-08-23 19:52:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-function addUser(){
-    player1_name = document.getElementById("player1_name_input").value;
-    player2_name = document.getElementById("player2_name_input").value;
-
-    localStorage.setItem("player1_name",player1_name);
-    localStorage.setItem("player2_name",player2_name);
-
-    window.location = "game_page.html";
-}
+function addUser() {
+    user_name = document.getElementById("user_name").value;  
+    localStorage.setItem("user_name",user_name);
+    window.location = "kwitter_room.html";
+  }
